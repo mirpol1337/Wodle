32d567d32d8eab7e1933758bcc3b25199b37f36e
nicaa <nicaa@MIREK>
2022-06-20 14:35:36 +0200

    Přidejte soubory projektu.

---
+++ Wodle/Program.cs
@@ -0,0 +1,26 @@
+﻿using Wodle.Entities;
+using Wodle.Services;
+
+namespace Wodle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prompt();
+            SecretService.Check();
+;
+        }
+        static void Prompt()
+        {
+            Console.WriteLine("Guess 5 letter Word");
+        }
+        static int LenghtPrompt()
+        {
+            int len = 0;
+            Console.WriteLine("How long word would you like to guess");
+            return len = Console.Read();
+        }
+
+    }
+}
---
+++ Wodle/Services/SecretService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wodle.Entities;
+
+namespace Wodle.Services
+{
+    public class SecretService
+    {
+        public static void Check()
+        {
+            string guess = Validate(guess: Console.ReadLine());
+            if (guess.Equals(Secret.Solution()))
+            {
+                Result(true);
+            }
+            else
+            {
+                Result(false);
+                Hint(guess);
+                Check();
+            }
+        }
+        static string Validate(string guess)
+        {
+            if (guess.Length != 5)
+            {
+                Console.WriteLine("Guess must contain 5 characters");
+                Check();
+            }
+            return guess.ToLowerInvariant();
+        }
+        static string Hint(string guess)
+        {
+            string hint = "";
+            for (int i = 0; i < Secret.Solution().Length; i++)
+            {
+                if (guess[i].Equals(Secret.Solution()[i]))
+                {
+                    hint += "Y";
+                }
+                else if (Contains(guess[i]))
+                {
+                    hint += "C";
+                }
+                else
+                {
+                    hint += "N";
+                }
+            }
+            Console.WriteLine(hint);
+            return hint;
+        }
+        static bool Contains(Char guess)
+        {
+
+            if (Secret.Solution().Contains(guess))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        static void Result(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("CORRECT");
+            }
+            else
+            {
+                Console.WriteLine("Guess Again");
+            }
+        }
+    }
+}
